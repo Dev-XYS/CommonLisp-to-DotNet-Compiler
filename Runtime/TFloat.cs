a8bfcd3fc3ca6e4815f7af0b88482bf035c91379
Dev-XYS <xiaoyushuo0419@live.com>
2020-03-19 21:14:01 +0800

    [runtime] add basic runtime types

---
+++ Runtime/TFloat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Runtime
+{
+    class TFloat
+    {
+        public double Value { get; set; }
+    }
+}
---
+++ Runtime/TString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Runtime
+{
+    class TString : IType
+    {
+        public string Value { get; set; }
+    }
+}
