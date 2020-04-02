2db9f0fa01f379c0ac524588b46df7ed19590c5c
Dev-XYS <xiaoyushuo0419@live.com>
2020-04-02 21:37:26 +0800

    [il] add label and jump instructions

---
+++ Compiler/IL/JumpInstruction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.IL
+{
+    abstract class JumpInstruction : IInstruction
+    {
+        public Label Target { get; }
+
+        public JumpInstruction(Label target)
+        {
+            Target = target;
+        }
+    }
+
+    class UnconditionalJumpInstruction : JumpInstruction
+    {
+        public UnconditionalJumpInstruction(Label target) : base(target)
+        {
+        }
+    }
+
+    class ConditionalJumpInstruction : JumpInstruction
+    {
+        public bool Condition { get; }
+
+        public ConditionalJumpInstruction(Label target, bool condition) : base(target)
+        {
+            Condition = condition;
+        }
+    }
+}
---
+++ Compiler/IL/Label.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.IL
+{
+    class Label : IInstruction
+    {
+        public string Name { get; set; }
+
+        public Label(string name)
+        {
+            Name = name;
+        }
+    }
+}
