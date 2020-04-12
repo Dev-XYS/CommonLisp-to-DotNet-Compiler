1a634e36dc9a7f0d88bccc5dd959f38899f0689a
Yukimai <yukimaikoriya@vip.qq.com>
2020-04-12 22:38:37 +0800

    [runtime] added EQ&EQL

---
+++ Runtime/Function/Equality.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Runtime.Function
+{
+    public class Eq : IType
+    {
+        public IType Invoke(IType[] args)
+        {
+            if (args.Length != 2) throw new RuntimeException("EQ: require exactly 2 arguments");
+            return args[0].Equals(args[1]) ? Lisp.t : Lisp.nil;
+        }
+    }
+    public class Eql : IType
+    {
+        public IType Invoke(IType[] args)
+        {
+            if (args.Length != 2) throw new RuntimeException("EQL: require exactly 2 arguments");
+            if (args[0].Equals(args[1])) return Lisp.t;
+            if(args[0] is Number x && args[1] is Number y)
+            {
+                return x.GetType() == y.GetType() && x.Equal(y) ? Lisp.t : Lisp.nil;
+            }
+            //if(args[0] is TChar x && args[1] is TChar y) //todo
+            return Lisp.nil;
+        }
+    }
+}
