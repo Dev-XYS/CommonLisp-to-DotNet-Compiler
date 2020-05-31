74da44b5adc83c1faf4a3498f8cc3e4fd28b58fe
Yukimai <yukimaikoriya@vip.qq.com>
2020-06-01 00:49:27 +0800

    [frontend] added library compile mode

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -5,14 +5,27 @@ namespace Compiler
 {
     class Program
     {
+        static void CompileLibrary()
+        {
+            var prog = Frontend.Core.CompileLibrary();
+            var cilprog = new CIL.Program(prog);
+            var sw = new System.IO.StreamWriter("Library.il");
+            cilprog.Emit(sw, CIL.EmissionType.Library);
+            sw.Close();
+            Assembler.Assembler.Invoke("Library.il");
+        }
         static void Main(string[] args)
         {
             IL.Program program;
-            if (args.Length > 0)
+            if (args.Length == 1)
             {
+                if(args[0] == "-l")
+                {
+                    CompileLibrary();
+                    return;
+                }
                 program = Frontend.Core.CompileFromFile(args[0]);
-            }
-            else
+            }else 
             {
                 program = Frontend.Core.CompileFromStdin();
             }
